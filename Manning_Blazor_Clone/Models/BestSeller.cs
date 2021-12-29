366426cbb39a01c923fdcb3b3fd6f0e35d4dd6f8
Ivan Ivanov <ivanov.ivan.v@gmail.com>
2021-12-29 03:04:50 +0200

    initial commit: - implemented BestSellers component

---
+++ Manning_Blazor_Clone/Models/BestSeller.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Manning_Blazor_Clone.Models
+{
+    public class BestSeller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [JsonPropertyName("meap")]
+        public bool IsMeap { get; set; }
+        public string Category { get; set; }
+        [JsonPropertyName("link")]
+        public string BookDetailUrlPage { get; set; }
+    }
+}
